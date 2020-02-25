94ea7eb0a6a267707f7c3a27dd8e4bb8144a0388
Swyter <swyterzone@gmail.com>
2020-02-26 00:01:04 +0100

    Seems to work so far now, disabled the actual ban for testing.

--- sphinx-bot.py
+++ sphinx-bot.py
@@ -17,6 +17,9 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+def exception_handler(loop, context):
+    print('Exception handler called', loop, context)
+
 # swy: exit if we don't have a valid bot token
 if not 'DISCORD_TOKEN' in os.environ:
   print('[!] Set your DISCORD_TOKEN environment variable to your Discord client secret.')
@@ -27,6 +30,11 @@ class SphinxDiscordClient(discord.Client):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     
+    #self.loop.set_exception_handler(exception_handler)
+    self.loop.set_debug(True)
+    # create the background task and run it in the background
+    self.bg_task = self.loop.create_task(self.checker_background_task())
+    
   async def on_ready(self):
     print('Logged in as')
     print(self.user.name)
@@ -36,36 +44,37 @@ class SphinxDiscordClient(discord.Client):
     self.moderation_log = self.get_channel(545777338130890752) # the moderation-log channel
     self.sphinx_guild   = self.get_guild  (409322660070424605) # Sphinx Community
     
-    #mem = self.sphinx_guild.members
-    #
-    #for m in mem:
-    #    time_since_creation = (m.joined_at - m.created_at)
-    #    seconds_since_creation = time_since_creation.total_seconds()
-    #    print(m.joined_at, m.bot, m.nick, m.name, m.discriminator, m.is_on_mobile(), time_since_creation, seconds_since_creation, "¨¨Possible bot" if (seconds_since_creation < 120) else "Not likely", m.avatar_url, m.id, m.is_avatar_animated(), m.activities)
-    #    
-    #    if (seconds_since_creation > 60*60):
-    #        continue
+    mem = self.sphinx_guild.members
+    for m in mem:
+        time_since_creation = (m.joined_at - m.created_at)
+        seconds_since_creation = time_since_creation.total_seconds()
+        #print(m.joined_at, m.bot, m.nick, m.name, m.discriminator, m.is_on_mobile(), time_since_creation, seconds_since_creation, "¨¨Possible bot" if (seconds_since_creation < 120) else "Not likely", m.avatar_url, m.id, m.is_avatar_animated(), m.activities)
+        
+        if (seconds_since_creation <= 60):
+            print("calling on", m)
+            await self.apply_ban_rules(member=m)
+    print("done waiting")
     
-  async def apply_ban_rules(member=None, user=None, on_member_join=False):
-  
+  async def apply_ban_rules(self, member=None, user=None, on_member_join=False):
+
     # swy: sanity check; ensure we either have an user or a member, buth not both
     assert((member and user == None) or (member == None and user))
-    
-    if (not member):
+
+    if not member:
         member = self.sphinx_guild.get_member(user.id)
 
-    if (not user):
+    if not user:
         user = self.get_user(member.id)
-  
-    time_since_creation    = (m.joined_at - m.created_at)
-    seconds_since_creation = time_since_creation.total_seconds()
-    print('User joined: ', pprint(member), time.strftime("%Y-%m-%d %H:%M"), member.avatar, member.created_at, "Seconds since account creation: " + str(seconds_since_creation))
-    
+        
+    print("--called", member)
     
     # swy: only apply these rules to unverified
     #      users as a basic safeguard
-    if len(member.roles):
-        return
+    #if len(member.roles) > 0:
+    #    return
+    print("--passed", member)
+    time_since_creation    = (member.joined_at - member.created_at)
+    seconds_since_creation = time_since_creation.total_seconds()
     
     reasons = []
     blacklisted_avatars = [
@@ -83,7 +92,7 @@ class SphinxDiscordClient(discord.Client):
     
     # swy: for some reason in newer ccounts there's a mismatch between the member avatar and the profile avatar.
     try:
-        usr = await self.fetch_user(m.id)
+        usr = await self.fetch_user(member.id)
     except discord.NotFound:
         print("ID %s is not a Discord user." % m.id)
         
@@ -92,7 +101,7 @@ class SphinxDiscordClient(discord.Client):
     
     #if member.hypesquad and (datetime.utcnow() - member.created_at).days <= 3:
     #    reasons.append("3 day-old account with HypeSquad.")
-    
+    print("reasons", reasons)
     if reasons:
         embed = discord.Embed(colour=discord.Colour(0x1b2148), title='Reasons', description=(" - " + "\n - ".join(reasons)))
         
@@ -108,19 +117,45 @@ class SphinxDiscordClient(discord.Client):
       
       
   async def on_member_join(self, member):
-    apply_ban_rules(member=member, on_member_join=True)
+    time_since_creation    = (member.joined_at - member.created_at)
+    seconds_since_creation = time_since_creation.total_seconds()
+    
+    print('User joined: ', pprint(member), time.strftime("%Y-%m-%d %H:%M"), member.avatar, member.created_at, "Seconds since account creation: " + str(seconds_since_creation))
+    await self.apply_ban_rules(member=member, on_member_join=True)
+  
   
   async def on_user_update(self, before, after):
-    # swy: only apply these rules to unverified users as a basic safeguard
     print(before, after)
     
-    if (before.avatar == after.avatar):
-        return
+    # swy: we only care about avatar changes
+    #if (before.avatar == after.avatar):
+    #    return
         
-    apply_ban_rules(user=after)    
+    await self.apply_ban_rules(user=after)
 
   async def on_message_delete(self, message):
     print('Deleted message:', pprint(message), message.content, time.strftime("%Y-%m-%d %H:%M"))
+
+  async def checker_background_task(self):
+    await self.wait_until_ready()
+    print('[i] background ban checker ready')
+    self.sphinx_guild   = self.get_guild  (409322660070424605) # Sphinx Community
+    
+    await asyncio.sleep(5)
+    
+    while not self.is_closed():
+        #mem = self.sphinx_guild.members
+        #for m in mem:
+        #    time_since_creation = (m.joined_at - m.created_at)
+        #    seconds_since_creation = time_since_creation.total_seconds()
+        #    #print(m.joined_at, m.bot, m.nick, m.name, m.discriminator, m.is_on_mobile(), time_since_creation, seconds_since_creation, "¨¨Possible bot" if (seconds_since_creation < 120) else "Not likely", m.avatar_url, m.id, m.is_avatar_animated(), m.activities)
+        #    
+        #    if (seconds_since_creation <= 60):
+        #        print("calling on", m)
+        #        await self.apply_ban_rules(member=m)
+        #print("done waiting")
+        # task runs every 30 seconds; infinitely
+        await asyncio.sleep(30 * 60)
 
 # swy: launch our bot thingie, allow for Ctrl + C
 client = SphinxDiscordClient()
@@ -131,6 +166,8 @@ while True:
   except KeyboardInterrupt:
     client.loop.run_until_complete(client.logout())
     # cancel all tasks lingering
+  except e:
+    print(e)
   finally:
     client.loop.close()
     sys.exit(0)
