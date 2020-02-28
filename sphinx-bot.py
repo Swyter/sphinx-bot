fbc726392e725aa1edfee344347e689c260b481b
Swyter <swyterzone@gmail.com>
2020-02-28 20:08:30 +0100

    Add startup time to the log. Tweak and improve some comments. Add a bunch of new bad avatars to the blacklist. Comment out some verbose debug prints now that we know that the thing seems to work more or less normally.

--- sphinx-bot.py
+++ sphinx-bot.py
@@ -39,19 +39,19 @@ class SphinxDiscordClient(discord.Client):
     
   async def on_ready(self):
     # swy: see checker_background_task() for the initialized "global" variables
-    print('Logged in as')
+    print('Logged in as | ' + time.strftime("%Y-%m-%d %H:%M"))
     print(self.user.name)
     print(self.user.id)
     print('------')
     
-    # swy: global variables for this client instance, this is loaded right after on_ready, so it's a good place()
+    # swy: global variables for this client instance, all the other background tasks wait for post_init_event, so it's a good place
     self.channel_test   = self.get_channel(470890531061366787) # Swyter test -- #general
-    self.moderation_log = self.get_channel(545777338130890752) # the moderation-log channel
+    self.moderation_log = self.get_channel(545777338130890752) # the #moderation-log channel
     self.post_init_event.set()
     
     
   async def apply_ban_rules(self, member=None, on_member_join=False, on_member_update=False):
-    # swy: sanity check; ensure we either have a member
+    # swy: sanity check; ensure that at least we have a member
     assert(member)
         
     # swy: only apply these rules to unverified users as a basic safeguard (they only have one role; called @everyone)
@@ -79,9 +79,11 @@ class SphinxDiscordClient(discord.Client):
         '67d05954883e8c5ba39f6e61ab681964', # https://cdn.discordapp.com/avatars/682013036659474434/67d05954883e8c5ba39f6e61ab681964.png?size=128
         '334b09e66c36ffbb0ea946d7641f1018', # https://cdn.discordapp.com/avatars/681774685377003527/334b09e66c36ffbb0ea946d7641f1018.png?size=128
         'd97d9ee1f09baa485ac06bc33ef644b2', # https://cdn.discordapp.com/avatars/681885340457369632/d97d9ee1f09baa485ac06bc33ef644b2.png?size=128
+        'fc3e70aec60efb5b7f48c9bf6c34db3a', # https://cdn.discordapp.com/avatars/682516445887987714/fc3e70aec60efb5b7f48c9bf6c34db3a.png?size=128
+        'f2f5fe1ad679ceb59787cb1b5853168f', # https://cdn.discordapp.com/avatars/682599331433545760/f2f5fe1ad679ceb59787cb1b5853168f.png?size=128
     ]
     
-    print("   Member status:", member.name, member.discriminator, member.id, member.status, member.mobile_status, member.desktop_status, member.web_status, member.activity, member.avatar, time_since_joining, seconds_since_joining)
+  # print("   Member status:", member.name, member.discriminator, member.id, member.status, member.mobile_status, member.desktop_status, member.web_status, member.activity, member.avatar, time_since_joining, seconds_since_joining)
     
     # swy: add a heartbeat detector during the first 120 seconds, to detect a modicum of user activity in new accounts
     if (member.id in self.member_heart and member.status is not discord.Status.offline and seconds_since_joining <= 120):
@@ -137,7 +139,7 @@ class SphinxDiscordClient(discord.Client):
     time_since_creation    = (member.joined_at - member.created_at)
     seconds_since_creation = time_since_creation.total_seconds()
     
-    # swy: last_member_joined improves the cadence of checker_background_task(),
+    # swy: last_member_joined improves the cadence of checker_background_task(), and with the post_join_event we can skip the wait.
     #      if member_heart is set to zero we know that the member joined in this session, as there is not persistent storage
     self.last_member_joined = datetime.utcnow()
     self.member_heart[member.id] = 0
@@ -207,6 +209,7 @@ class SphinxDiscordClient(discord.Client):
             for guild in self.guilds:
                 m = guild.get_member(message.author.id)
                 if m and any(x in str(m.roles) for x in ['THQ Nordic', 'Titan (Owners)', 'Pharaohs (Admins)', 'Demigods (Mods)']):
+                    await message.add_reaction('👌')
                     await message.channel.send("Disengaging.")
                     await self.logout()
         
@@ -234,11 +237,11 @@ class SphinxDiscordClient(discord.Client):
                 seconds_since_creation = time_since_creation.total_seconds()
 
                 if (seconds_since_creation <= 60 * 60 and len(m.roles) <= 1):
-                    print("ssc", m.name, m.discriminator, seconds_since_creation, time_since_creation, m.joined_at, m.created_at)
+                  # print("ssc", m.name, m.discriminator, seconds_since_creation, time_since_creation, m.joined_at, m.created_at)
                     await self.apply_ban_rules(member=m)
 
             # task runs every 30 seconds; infinitely. but run at a faster rate right after someone joins to try to get more heartbeats
-            print("cadence", datetime.utcnow(), self.last_member_joined, (datetime.utcnow() - self.last_member_joined), (datetime.utcnow() - self.last_member_joined).total_seconds())
+          # print("cadence", datetime.utcnow(), self.last_member_joined, (datetime.utcnow() - self.last_member_joined), (datetime.utcnow() - self.last_member_joined).total_seconds())
             if (datetime.utcnow() - self.last_member_joined).total_seconds() < 30:
                 cadence = 1
             else:
@@ -257,7 +260,7 @@ while True:
     client.loop.run_until_complete(client.start(os.environ["DISCORD_TOKEN"]))
   except KeyboardInterrupt:
     client.loop.run_until_complete(client.logout())
-    # cancel all tasks lingering
+    # cancel all lingering tasks
   except e:
     print(e)
   finally:
