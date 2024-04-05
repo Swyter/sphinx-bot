18fd19960e4b7d175a1a99b24294389bded5f725
Swyter <swyterzone@gmail.com>
2024-04-05 02:02:19 +0200

    Add further comments, remove redundant unverified_role retrieval and clean it up another bit. No functional changes intended.

--- sphinx-bot.py
+++ sphinx-bot.py
@@ -25,7 +25,8 @@ if not 'DISCORD_TOKEN' in os.environ:
   print('[!] Set your DISCORD_TOKEN environment variable to your Discord client secret.')
   sys.exit(-1)
 
-
+# --
+# swy: this discord.py cog includes a silly way of chatting with the babbling lizard, as well as offering a way to kill the bot completely.
 class SphinxDiscordGoofyLizard(discord.ext.commands.Cog):
   def __init__(self, bot: discord.ext.commands.Bot, log_to_channel):
     self.bot = bot
@@ -46,7 +47,7 @@ class SphinxDiscordGoofyLizard(discord.ext.commands.Cog):
     if isinstance(message.channel, discord.DMChannel):
         print("PM:", pprint(message), message.content, time.strftime("%Y-%m-%d %H:%M"))
         
-        # swy: add an emergency killswitch
+        # swy: add an emergency killswitch; make it only work on selected moderation roles
         if message.content.lower().startswith('please stop'):
             for guild in self.bot.guilds:
                 m = guild.get_member(message.author.id)
@@ -67,7 +68,8 @@ class SphinxDiscordGoofyLizard(discord.ext.commands.Cog):
             await asyncio.sleep(random.randint(2, 6))
             await message.channel.send(msg)
 
-
+# --
+# swy: this discord.py cog applies an «Unverified» role to any account that properly joins the server (i.e. not )
 # swy: the final question will have three good and three bad answers, so have some extras of each to mix them up
 questions = [
   {'question': 'Which of these characters belong to the game?',   'answers_good': ["Sphinx", "Tutankhamen", "Imhotep", "Horus", "Nefertiti"],                                                                                 'answers_bad': ["Link (the princess)", "Zelda (the guy with the sword)", "Jade", "The Four Horsemen", "Amaterasu", "Jak", "Daxter"       ] },
@@ -80,7 +82,6 @@ class TldDiscordValidator(discord.ext.commands.Cog):
   def __init__(self, bot: discord.ext.commands.Bot, log_to_channel):
     self.bot = bot
     self.log_to_channel = log_to_channel
-
     print('[i] portal-god validator plug-in ready')
 
   @discord.ext.commands.Cog.listener()
@@ -88,6 +89,7 @@ class TldDiscordValidator(discord.ext.commands.Cog):
     self.channel_test = self.bot.get_channel( 470890531061366787) # Swyter test -- #general
     self.channel_door = self.bot.get_channel(1225464253415424022) # Sphinx and the Cursed Mummy -- #portal-god
 
+    self.unverified_role = discord.utils.get(self.channel_door.guild.roles, name="Unverified")
     self.kick_stuck_members.start()
 
     # swy: there's a permanent message with a button (TldVerifyPresentation), when clicking it we
@@ -96,7 +98,7 @@ class TldDiscordValidator(discord.ext.commands.Cog):
         def __init__(self):
           super().__init__(timeout=None)
           self.add_item(discord.ui.Button(label="Visit the Steam Community page", style=discord.ButtonStyle.link, url="https://steamcommunity.com/app/606710"))
-          #self.add_item(discord.ui.Button(label="Submit bug reports",             style=discord.ButtonStyle.link, url="https://redmine.thqnordic.com/news/15"))
+#         self.add_item(discord.ui.Button(label="Submit bug reports",             style=discord.ButtonStyle.link, url="https://redmine.thqnordic.com/news/15"))
 
         @discord.ui.button(label="Verify my account", style=discord.ButtonStyle.blurple, custom_id='tld:verify')
         async def blurple_button(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -123,13 +125,13 @@ class TldDiscordValidator(discord.ext.commands.Cog):
                 super().__init__(timeout=30)
                 self.rand_answers_good = rand_answers_good
 
+              # swy: make a selector box with three good and three bad selectable options
               @discord.ui.select(placeholder=question_text, min_values=3, max_values=3, options=ans_options)
               async def select_menu(self, interaction: discord.Interaction, select: discord.ui.Select):
                 print("click")
 
                 # swy: are all the options correct? even one bad one will cause it to fail
                 if len(set(select.values).intersection(rand_answers_good)) != len(rand_answers_good):
-                  self.is_finished=True
                   await interaction.response.send_message(f"Darn, try again!", ephemeral=True)
                   await client.log_to_channel(interaction.user, f"has **failed** validation by responding {select.values}.")
                   return
@@ -137,10 +139,8 @@ class TldDiscordValidator(discord.ext.commands.Cog):
                 await interaction.response.send_message(f"Awesome! I like {select.values[0]} too!\nNow you are in. Head over to {interaction.guild.rules_channel.mention}.", ephemeral=True)
 
                 # swy: unquarantine the user by getting rid of this role
-                unverified_role = discord.utils.get(interaction.guild.roles, name="Unverified")
-
-                if unverified_role:
-                  await interaction.user.remove_roles(unverified_role)
+                if self.unverified_role:
+                  await interaction.user.remove_roles(self.unverified_role)
 
                 await client.log_to_channel(interaction.user, f"has **passed** validation by responding {rand_answers_good}.")
 
@@ -155,41 +155,42 @@ class TldDiscordValidator(discord.ext.commands.Cog):
     # swy: make the first post's buttons persistent across bot reboots
     self.bot.add_view(TldVerifyPresentation())
 
-    #await self.channel_door.send(
-    #  "As much as the team hates to do this, we've been receiving too much spam from new accounts lately. 🐧\n" +
-    #  "So we need to make sure you are a real person to let you in. Pretty easy; a one-question quiz about *the game and Egyptian stuff*!", view=TldVerifyPresentation()
-    #)
+    # swy: only make it send this message the first time around, disable it after that
+    '''
+    await self.channel_door.send(
+      "As much as the team hates to do this, we've been receiving too much spam from new accounts lately. 🐧\n" +
+      "So we need to make sure you are a real person to let you in. Pretty easy; a one-question quiz about *the game and Egyptian stuff*!", view=TldVerifyPresentation()
+    )
+    '''
 
   @discord.ext.commands.Cog.listener()
   async def on_member_join(self, member : discord.Member):
     print('User joined: ', pprint(member), time.strftime("%Y-%m-%d %H:%M"))
     await client.log_to_channel(member, f" has **joined**. Account created at {member.created_at}. Quarantining and adding *Unverified* role.")
 
-    unverified_role = discord.utils.get(member.guild.roles, name="Unverified")
-
-    if unverified_role:
-      await member.add_roles(unverified_role)
+    if self.unverified_role:
+      await member.add_roles(self.unverified_role)
       mes = await self.channel_door.send(f"{member.mention}") # swy: ping them to make the hidden channel pop up more
       await mes.delete(delay=2) # swy: phantom ping
 
   @discord.ext.tasks.loop(seconds=30)
   async def kick_stuck_members(self):
-    guild = self.channel_door.guild
-    unverified_role = discord.utils.get(guild.roles, name="Unverified")
-    
-    for member in unverified_role.members:
+    for member in self.unverified_role.members:
       # swy: ignore users (with more roles than just this and @everyone) that may have this role for testing or to mess around
       if len(member.roles) > 2:
         continue
 
+      # swy: if the member with the «Unverified» role has idled more than 10 minutes in the gate since
+      #      joining, then kick them out; they can still safely rejoin later if they want to :-)
       then = member.joined_at; now = datetime.datetime.now(datetime.timezone.utc)
 
       if (now - then) > datetime.timedelta(minutes=10):
         await client.log_to_channel(member, f"is getting **kicked** for being on quarantine for too long.")
         await member.kick(reason='bot: waited too long before passing the test')
 
-
-# swy: implement our bot thingie
+# --
+# swy: implement our base discord.py bot thingie; it hosts the "cogs" we can attach to add extra functionality
+#      it doesn't really do anything else by itself, other than having a common audit channel log function
 class SphinxDiscordClient(discord.ext.commands.Bot):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
@@ -212,8 +213,6 @@ class SphinxDiscordClient(discord.ext.commands.Bot):
     
     # swy: global variables for this client instance, all the other background tasks wait for post_init_event, so it's a good place
     self.channel_test   = self.get_channel( 470890531061366787) # Swyter test -- #general
-    self.moderation_log = self.get_channel( 545777338130890752) # the #moderation-log channel
-    
     self.portal_god     = self.get_channel(1225464253415424022) # the #portal-god channel 
     self.portal_god_log = self.get_channel(1225486542597001316) # the #portal-god-log channel 
 
@@ -221,7 +220,6 @@ class SphinxDiscordClient(discord.ext.commands.Bot):
     print(user, text)
     if self.portal_god_log:
       await self.portal_god_log.send(f"{user.mention} `{user.name}#{user.discriminator} ({user.id})` {text}")
-    
 # --
 
 intents = discord.Intents.default()
@@ -234,7 +232,7 @@ loop = asyncio.get_event_loop()
 def handle_exit():
     raise KeyboardInterrupt
 
-if os.name != 'nt':
+if os.name != 'nt': # swy: if this isn't running on Windows (i.e. Linux/UNIX/macOS)
   loop.add_signal_handler(signal.SIGTERM, handle_exit, signal.SIGTERM)
   loop.add_signal_handler(signal.SIGABRT, handle_exit, signal.SIGABRT, None)
 
